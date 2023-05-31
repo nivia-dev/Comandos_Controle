6cf64fe95819f9e1a54d207d7adf46903b91de64
Nivia <niviafalves@hotmail.com>
2023-05-31 15:06:54 -0300

    Atualização
    Editando o Readme e refazendo o terceiro exercício

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Calculo_media
+# Comandos de controle
  ## ExercÍcio do curso DESCODIFICADAS
+
+ ## Objetivo
+ * Fazer um programa JavaScript que receba 3 números informados pelo usuário e em seguida apresente uma mensagem de alerta(alert) informando qual é o maior número dentre os números informados. 
+
+* Fazer um programa JavaScript em que o usuário informará a sua idade e o programa emitirá uma mensagem(alert) dizendo se o usuário é ou não menor de idade. 
+
+* Fazer um programa JavaScript que encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000. Mostre os resultados em um site.
--- script.js
+++ script.js
@@ -24,30 +24,17 @@ function maioridade(){
 
 
 function multiplos(){
-    let mult3 = 0;
-    let mult5 = 0;
-    let mult3e5 = 0;
-    let res3 = document.getElementById('res3');
-    let res5 = document.getElementById('res5');
-    let res3e5 = document.getElementById('res3e5');
-    
+    let soma = 0;
+    let res = document.getElementById('res');
+       
     
     for (i =0; i <= 1000; i++ ) {
-        if (i % 3 === 0) {
-            mult3 += i;
-        }
-
-        if(i % 5 === 0) {
-            mult5 += i;    
-        }
-
-        if(i % 3 === 0 && i % 5 === 0) {
-            mult3e5 += i;   
+        if(i % 3 === 0 || i % 5 === 0) {
+            soma += i;   
         }
         
-        res3.innerHTML = `A soma dos multiplos de 3 é <strong>${mult3}</strong>`
-        res5.innerHTML = `A soma dos multiplos de 5 é <strong>${mult5}</strong>`
-        res3e5.innerHTML = `A soma dos multiplos de 3 que também <br> são multiplos de 5 é <strong>${mult3e5}</strong>`
+        res.innerHTML = `A soma dos multiplos de 3 ou 5 é <strong>${soma}</strong>`
+        
     }
     
    
